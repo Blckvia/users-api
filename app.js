3de704568a1ce90b2ea0da93f52a7f97ac07cb22
blckvia <a.vyatkinn@icloud.com>
2023-03-02 20:09:16 +0500

    feat: multer added with limits

--- app.js
+++ app.js
@@ -2,16 +2,40 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const sequelize = require('./util/database');
-
-const User = require('./models/user');
 
 const userRoutes = require('./routes/users');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const fileSize = parseInt(req.headers['content-length']);
+  if (
+    file.mimetype === 'image/png' ||
+    (file.mimetype === 'image/jpg' && fileSize <= 1048576)
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', function (req, res) {
